f486b14ceefe22c40105850862cd79ceb8f62f1f
wtoduo <wtoduo@wtoduodeMacBook-Air.local>
2019-06-18 22:24:39 +0800

    初步实现底部item与body内容的关联及item切换

---
+++ lib/hot/hot_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HotWidget extends StatefulWidget {
+  @override
+  _HotWidgetState createState() => _HotWidgetState();
+}
+
+class _HotWidgetState extends State<HotWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("热映"),
+    );
+  }
+}
---
+++ lib/movies/movies_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesWidget extends StatefulWidget {
+  @override
+  _MoviesWidgetState createState() => _MoviesWidgetState();
+}
+
+class _MoviesWidgetState extends State<MoviesWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("找片"),
+    );
+  }
+}
