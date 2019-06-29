98c87cc4ffa357068b5f7b9a1462bf6a6e19f537
wtowto7207 <wtowto7207@163.com>
2019-06-29 09:55:21 +0800

    commit

--- lib/citys/citys.dart
+++ lib/citys/citys.dart
@@ -7,6 +7,23 @@ class CitysWidget extends StatefulWidget {
 
 class _CitysWidgetState extends State<CitysWidget> {
   String curCity;
+  List<String> hotCitys = [
+    '北京',
+    '上海',
+    '广州',
+    '深圳',
+    '成都',
+    '武汉',
+    '杭州',
+    '重庆',
+    '郑州',
+    '南京',
+    '西安',
+    '苏州',
+    '长沙',
+    '天津',
+    '福州'
+  ];
   @override
   Widget build(BuildContext context) {
     curCity = ModalRoute.of(context).settings.arguments;
@@ -50,8 +67,105 @@ class _CitysWidgetState extends State<CitysWidget> {
               child: Container(
                 child: TabBarView(
                   children: <Widget>[
-                    Center(
-                      child: Text('国内'),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        // 搜索框
+                        Container(
+                          height: 50.0,
+                          child: TextField(
+                            textAlign: TextAlign.start,
+                            cursorColor: Colors.green,
+                            decoration: InputDecoration(
+                              hintText: '输入城市名称查询',
+                              hintStyle: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.black38,
+                              ),
+                              border: InputBorder.none,
+                              filled: true,
+                              fillColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                        // GPS定位城市
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 5.0, bottom: 5.0, left: 20.0),
+                          child: Text(
+                            'GPS定位城市',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.black26,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 5.0, bottom: 5.0, left: 20.0),
+                          child: MaterialButton(
+                            child: Container(
+                              width: 70.0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Colors.green,
+                                    size: 18.0,
+                                  ),
+                                  Text(
+                                    curCity,
+                                  )
+                                ],
+                              ),
+                            ),
+                            color: Colors.white,
+                            elevation: 0.0,
+                            onPressed: () {},
+                          ),
+                        ),
+                        // 热门城市
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 5.0, bottom: 5.0, left: 20.0),
+                          child: Text(
+                            '热门城市',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.black26,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
+                            child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                childAspectRatio: 2.6,
+                                mainAxisSpacing: 20.0,
+                                crossAxisSpacing: 50.0,
+                              ),
+                              itemCount: hotCitys.length,
+                              itemBuilder: (context, index) {
+                                return MaterialButton(
+                                  color: Colors.white,
+                                  elevation: 0,
+                                  child: Text(hotCitys[index]),
+                                  onPressed: () {},
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     Center(
                       child: Text('海外'),
